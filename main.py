e98048bc13d991063aeb1c8423a3326aa6db0470
erlingto <erling.tmte@gmail.com>
2020-03-15 20:02:58 +0100

    dabbling

--- main.py
+++ main.py
@@ -6,17 +6,50 @@ import numpy as np
 gym = environment.ConnectXGym()
 net = network.ConnectXNetwork(gym.positions.n, gym.actions.n)
 
+gamma = 0.99
+copy_step = 25
+max_exp = 10000
+min_exp = 100
+batch_size = 32
+learning_rate = 1e-2
+epsilon = 0.5
+decay = 0.9999
+min_epsilon = 0.1
+episodes = 200000
+
+precision = 7
+
+gym = environment.ConnectXGym()
+num_states = gym.positions.n+1
+print("num_states", num_states)
+num_actions = gym.actions.n
+TrainNet = environment.DQN(num_states, num_actions, gamma, max_exp, min_exp, batch_size, learning_rate)
+TargetNet = environment.DQN(num_states, num_actions, gamma, max_exp, min_exp, batch_size, learning_rate)
+observations = gym.reset()
+print(TrainNet.model)
+done =  False
+while not done:
+    action = TrainNet.get_action(observations, epsilon)
+
+    prev_observations = observations
+
+    observations, reward, done, _ = gym.step(action)
+
+    if done:
+        print(reward)
+        gym.render()
+        break
+    
+    
+'''
 observation = gym.trainer.reset()
-board = np.matrix(observation.board)
-board = board.reshape(gym.columns, gym.rows)
-board = torch.from_numpy(board)
-board.unsqueeze_(-1)
-print(board)
-
-
+board = np.array(observation.board)
+board = torch.from_numpy(board).float()
 
 while not gym.env.done:
-    my_action = max(net.forward(board))
-    observation, reward, done, info = gym.trainer.step(my_action)
-    board = torch.from_numpy(np.matrix(observation.board))
+    value, my_action = torch.max(net.forward(board).float(),0)
+    observation, reward, done, info = gym.trainer.step(my_action.item())
+    board = torch.from_numpy(np.array(observation.board)).float()
     gym.env.render()
+'''
+
--- network.py
+++ network.py
@@ -10,18 +10,13 @@ from kaggle_environments import evaluate, make
 class ConnectXNetwork(nn.Module):
     def __init__(self, num_states, num_actions):
         super(ConnectXNetwork, self).__init__()
-        self.conv1 = nn.Conv1d(6, 16, 1)
-        self.conv2 = nn.Conv1d(16, 16, 1)
-        self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(num_states, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_actions)
     
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))    
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+    def forward(self, x):   
+        x = F.sigmoid(self.fc1(x))
+        x = F.sigmoid(self.fc2(x))
         x = self.fc3(x)
         return x 
     
